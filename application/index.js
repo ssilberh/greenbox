ed1514033803797cc7955f384cc0e7cd3a166be9
Steven <steven.silberholz@gmail.com>
2018-02-24 22:00:22 -0500

    add more routing for getting/creating boxes, users

--- application/index.js
+++ application/index.js
@@ -4,17 +4,40 @@ const express = require('express')
 const parser = require('body-parser')
 const app = express()
 
+var greenboxes = {};
+
 app.use(parser.urlencoded({ extended: false }))
 app.use(parser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/greenbox', (req, res) =>
-  res.send('Greenbox created or updated ' + req.body.id)
-)
+// create a greenbox
+app.post('/greenbox', function (req, res) {
+  var id = req.body.id;
+  console.log('Attempting to add box '+id);
 
-app.post('/user', (req, res) =>
+  greenboxes.id = req.body;
+  res.send('Greenbox created or updated ' + id)
+})
+
+// get a greenbox
+app.get('/greenbox/:boxId', function (req, res) {
+  var id = req.params.boxId;
+  console.log('Attempting to get box with id '+id);
+  
+  var requestedGreenbox = greenboxes.id;
+  res.send('Got Greenbox: ' + JSON.stringify(requestedGreenbox));
+})
+
+// create a user
+app.post('/user', function (req, res) {
   res.send('User created or updated ' + req.body.id)
-)
+})
+
+// get a user
+app.get('/user', function (req, res) {
+  res.send('User created or updated ' + req.body.id)
+})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
