83c79d3d6c6bf3c111e5288a92c311217434bf5e
Steven <steven.silberholz@gmail.com>
2018-03-15 16:18:43 -0400

    add chartjs, attempt to integrate it on greenbox directive

--- web-client/components/greenbox.js
+++ web-client/components/greenbox.js
@@ -9,6 +9,28 @@ angular.module('greenboxModule',[])
     controller: ['$scope', function(scope) {
       scope.editingEnabled = false;
 
+      var data = {
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+          {
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,59,90,81,56,55,40]
+          },
+          {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : [28,48,40,19,96,27,100]
+          }
+        ]
+      }
+
+      scope.temperatureGraphOneYear = data;
+
       scope.enableEditing = function(box) {
         scope.editingEnabled = true;
       }
