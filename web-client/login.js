10755719b07f010ea506ce7ee158b420c6d60951
Steven <steven.silberholz@gmail.com>
2018-03-15 13:44:09 -0400

    add login/main splash page

--- web-client/login.js
+++ web-client/login.js
@@ -5,6 +5,23 @@ function parallax(){
     $('.jumbotronbg').css('height', ($('.jumbotron').outerHeight()-scrolled) + 'px');
 }
 
+$(".jumbotronbutton").on('click', function(e) {
+console.log("doing scroll?")
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = this.hash;
+     });
+
+});
+
 $(window).scroll(function(e){
     parallax();
 });
